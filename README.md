5232d2ca0d0afd979e4fdbe029b358d0a4d05b3d
Haoran Li <haorli@gl-login1.arc-ts.umich.edu>
2023-02-27 14:39:21 -0500

    release

--- README.md
+++ README.md
@@ -16,71 +16,59 @@ pip install --upgrade pip
 pip install -r requirements.txt
 ```
 ## Isoform quantification by Mini
+### Quantify using long reads data
 ```
-usage: main.py quantify [-h] -gtf GTF_ANNOTATION_PATH -lrsam
-                        LONG_READ_SAM_PATH -o OUTPUT_PATH
-                        [-srsam SHORT_READ_SAM_PATH]
-                        [-srfastq SHORT_READ_FASTQ]
-                        [-sr_m1 SHORT_READ_MATE1_FASTQ]
-                        [-sr_m2 SHORT_READ_MATE2_FASTQ]
-                        [-ref_genome REFERENCE_GENOME]
-                        [--SR_quantification_option SR_QUANTIFICATION_OPTION]
-                        [--alpha ALPHA] [--beta BETA] [--filtering FILTERING]
-                        [--multi_mapping_filtering MULTI_MAPPING_FILTERING]
-                        [--training TRAINING] [--DL_model DL_MODEL]
-                        [--assign_unique_mapping_option ASSIGN_UNIQUE_MAPPING_OPTION]
-                        [-t THREADS]
+python isoform_quantification/main.py quantify \
+-gtf GTF_ANNOTATION_PATH \
+-lrsam LONG_READS_SAM_PATH \
+-t 1 \
+-o OUTPUT_PATH
 
-optional arguments:
-  -h, --help            show this help message and exit
-
-required named arguments for isoform quantification:
+arguments:
   -gtf GTF_ANNOTATION_PATH, --gtf_annotation_path GTF_ANNOTATION_PATH
-                        The path of annotation file
+                        The path of isoform annotation file in GTF format
   -lrsam LONG_READ_SAM_PATH, --long_read_sam_path LONG_READ_SAM_PATH
-                        The path of long read sam file
+                        The path of long read sam file mapping to reference genome.
+  -t THREADS, --threads THREADS
+                        Number of threads. Default is 1.
   -o OUTPUT_PATH, --output_path OUTPUT_PATH
                         The path of output directory
-
-optional arguments:
-  -srsam SHORT_READ_SAM_PATH, --short_read_sam_path SHORT_READ_SAM_PATH
-                        The path of short read sam file
-  -srfastq SHORT_READ_FASTQ, --short_read_fastq SHORT_READ_FASTQ
-                        The path of short read fastq file
-  -sr_m1 SHORT_READ_MATE1_FASTQ, --short_read_mate1_fastq SHORT_READ_MATE1_FASTQ
-                        The path of short read mate 1 fastq file
-  -sr_m2 SHORT_READ_MATE2_FASTQ, --short_read_mate2_fastq SHORT_READ_MATE2_FASTQ
-                        The path of short read mate 2 fastq file
-  -ref_genome REFERENCE_GENOME, --reference_genome REFERENCE_GENOME
-                        The path of reference genome file
-  --SR_quantification_option SR_QUANTIFICATION_OPTION
-                        SR quantification option[Options: Mili,
-                        Kallisto,Salmon, RSEM] [default:Kallisto]
-  --alpha ALPHA         Alpha[default:adaptive]: SR and LR balance parameter
-  --beta BETA           Beta[default:1e-6]: L2 regularization parameter
-  --filtering FILTERING
-                        Whether the very short long reads will be
-                        filtered[default:False][True,False]
-  --multi_mapping_filtering MULTI_MAPPING_FILTERING
-                        How to filter multi-mapping
-                        reads[default:best][unique_only,best]
-  --training TRAINING   Generate training dict
-  --DL_model DL_MODEL   DL model to use
-  --assign_unique_mapping_option ASSIGN_UNIQUE_MAPPING_OPTION
-                        How to assign unique mapping reads
-                        [Options:linear_model,manual_assign]
-                        [default:linear_model]
-  -t THREADS, --threads THREADS
-                        Number of threads
-
 ```
-The alpha and beta parameters can be set as float. <br>
-The `alpha` should be set between 0 and 1, where 0 indicates using only the short reads and 1 indicates using only the long reads for quantification. <br>
-The `beta` can be set as a small float between 1e-9 to 1e-2. <br>
-By leaving `alpha` and `beta` default(adaptive), the alpha and beta will be obtained by deep learning model to get the optimal performance. <br>
-The `multi_mapping_filtering` can be set to `unique_only`, which will use only uniquely-mapping reads; or `best`, which will use the best MAPQ alignment from the multi-mapping reads; or do not set, which will not perform any filtering on multi_mapping reads. Default is `best`<br>
-Set `SR_quantification_option` to use other tools for short reads quantification. For other tools, you need also provide `reference_genome` and `srfastq` for single end reads / `sr_m1` and `sr_m2` for paired end reads.
-Set `assign_unique_mapping_option` to decide how to assign reads that are uniquely mapped to isoform. Set `linear_model` to use linear model to assign. Set `manual_assign` to assign manually.
+
+### Quantify using short and long reads data in hybrid mode
+```
+python isoform_quantification/main.py quantify \
+-gtf GTF_ANNOTATION_PATH \
+-lrsam LONG_READS_SAM_PATH \
+-srsam SHORT_READS_SAM_PATH \
+--EM_choice hybrid \
+-t 1 \
+-o OUTPUT_PATH
+
+arguments:
+  -gtf GTF_ANNOTATION_PATH, --gtf_annotation_path GTF_ANNOTATION_PATH
+                        The path of isoform annotation file in GTF format
+  -lrsam LONG_READ_SAM_PATH, --long_read_sam_path LONG_READ_SAM_PATH
+                        The path of long read sam file mapping to reference genome.
+  -srsam SHORT_READ_SAM_PATH, --short_read_sam_path SHORT_READ_SAM_PATH
+                        The path of short read sam file mapping to reference transcriptome.
+  -t THREADS, --threads THREADS
+                        Number of threads. Default is 1.
+  -o OUTPUT_PATH, --output_path OUTPUT_PATH
+                        The path of output directory
+```
+### Advanced parameters for hybrid quantification
+```
+optional arguments
+  --iter_theta ITER_THETA
+                        Whether use updated theta to re-calculate conditional probablity [True,False]. See the online methods for detailed information. Default is False to speed up the calculation.
+  --eff_len_option EFF_LEN_OPTION
+                        How to calculate the effective length [Kallisto,RSEM]. Choose Kallisto or RSEM to calculate the effective length in the same way as the corresponding method. Default is Kallisto.
+  --EM_SR_num_iters EM_SR_NUM_ITERS
+                        Number of maximum iterations for EM algorithm. Default is 200.
+  --inital_theta INITAL_THETA
+                        Inital_theta [LR,SR]. Set the initial theta based on the isoform expression given long reads(LR) or short reads (SR). Default is LR.
+```
 
 ## Calculate K-value
 ```
--- isoform_quantification/main.py
+++ isoform_quantification/main.py
@@ -15,7 +15,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Isoform quantification tools",add_help=True)
     subparsers = parser.add_subparsers(help='sub-command help',dest="subparser_name")
     parser_TrEESR = subparsers.add_parser('cal_K_value', aliases=['TrEESR'],help='Calculate K values')
-    parser_TransELS = subparsers.add_parser('quantify', aliases=['TransELS'],help='Isoform quantification')
+    # parser_TransELS = subparsers.add_parser('quantify', aliases=['TransELS'],help='Isoform quantification')
     
     requiredNamed_TrEESR = parser_TrEESR.add_argument_group('required named arguments for calculation of K value')
     requiredNamed_TrEESR.add_argument('-gtf','--gtf_annotation_path', type=str, help="The path of annotation file",required=True)
@@ -39,38 +39,38 @@ def parse_arguments():
     # assert os.path.exists(weight_path)
     # optional_TrEESR.add_argument('--region_weight_path',type=str, default=weight_path,help="Mili LR region weight path")
 
-    requiredNamed_TransELS = parser_TransELS.add_argument_group('required named arguments for isoform quantification')
-    requiredNamed_TransELS.add_argument('-gtf','--gtf_annotation_path', type=str, help="The path of annotation file",required=True)
-    requiredNamed_TransELS.add_argument('-lrsam','--long_read_sam_path', type=str, help="The path of long read sam file",required=True)
-    requiredNamed_TransELS.add_argument('-o','--output_path', type=str, help="The path of output directory",required=True)
+    # requiredNamed_TransELS = parser_TransELS.add_argument_group('required named arguments for isoform quantification')
+    # requiredNamed_TransELS.add_argument('-gtf','--gtf_annotation_path', type=str, help="The path of annotation file",required=True)
+    # requiredNamed_TransELS.add_argument('-lrsam','--long_read_sam_path', type=str, help="The path of long read sam file",required=True)
+    # requiredNamed_TransELS.add_argument('-o','--output_path', type=str, help="The path of output directory",required=True)
     
-    optional_TransELS = parser_TransELS.add_argument_group('optional arguments')
-    optional_TransELS.add_argument('-srsam','--short_read_sam_path', type=str, help="The path of short read sam file",default=None)
-    optional_TransELS.add_argument('-srfastq','--short_read_fastq', type=str, help="The path of short read fastq file",default=None)
-    optional_TransELS.add_argument('-sr_m1','--short_read_mate1_fastq', type=str, help="The path of short read mate 1 fastq file",default=None)
-    optional_TransELS.add_argument('-sr_m2','--short_read_mate2_fastq', type=str, help="The path of short read mate 2 fastq file",default=None)
+    # optional_TransELS = parser_TransELS.add_argument_group('optional arguments')
+    # optional_TransELS.add_argument('-srsam','--short_read_sam_path', type=str, help="The path of short read sam file",default=None)
+    # optional_TransELS.add_argument('-srfastq','--short_read_fastq', type=str, help="The path of short read fastq file",default=None)
+    # optional_TransELS.add_argument('-sr_m1','--short_read_mate1_fastq', type=str, help="The path of short read mate 1 fastq file",default=None)
+    # optional_TransELS.add_argument('-sr_m2','--short_read_mate2_fastq', type=str, help="The path of short read mate 2 fastq file",default=None)
 
-    optional_TransELS.add_argument('-ref_genome','--reference_genome', type=str, help="The path of reference genome file",default=None)
-    optional_TransELS.add_argument('--SR_quantification_option', type=str, help="SR quantification option[Options: Mili, Kallisto,Salmon, RSEM] [default:Kallisto]",default='Kallisto')
-    # optional_TransELS.add_argument('--kallisto_index', type=str, help="Kallisto index",default='/fs/project/PCON0009/Yunhao/Project/Mili/Annotation/KallistoIndex/gencode.v39.transcripts.clean.dedup.m')
-    optional_TransELS.add_argument('--alpha',type=str,default='adaptive', help="Alpha[default:adaptive]: SR and LR balance parameter")
-    optional_TransELS.add_argument('--beta',type=str, default='1e-6',help="Beta[default:1e-6]: L2 regularization parameter")
-    optional_TransELS.add_argument('--filtering',type=str,default='False', help="Whether the very short long reads will be filtered[default:False][True,False]")
-    optional_TransELS.add_argument('--multi_mapping_filtering',type=str,default='best', help="How to filter multi-mapping reads[default:best][unique_only,best]")
-    optional_TransELS.add_argument('--training',type=str,default='False', help="Generate training dict")
-    optional_TransELS.add_argument('--DL_model',type=str,default=None,help='DL model to use')
-    optional_TransELS.add_argument('--assign_unique_mapping_option',type=str,default='linear_model',help='How to assign unique mapping reads [Options:linear_model,manual_assign] [default:linear_model]')
-    optional_TransELS.add_argument('-t','--threads',type=int, default=1,help="Number of threads")
-    optional_TransELS.add_argument('--READ_JUNC_MIN_MAP_LEN',type=int, default=1,help="minimum mapped read length to consider a junction")
-    optional_TransELS.add_argument('--use_weight_matrix',type=str, default='True',help="Whether use weight matrix[default:True][True,False]")
-    optional_TransELS.add_argument('--normalize_lr_A',type=str, default='True',help="Whether normalize lr A [default:True] [True,False]")
-    optional_TransELS.add_argument('--same_struc_isoform_handling',type=str, default='merge',help="How to handle isoforms with same structures within a gene[default:merge][merge,keep]")
-    optional_TransELS.add_argument('--add_full_length_region',type=str, default='all',help="Whether add full length region[default:all] [all,nonfullrank,none]")
-    optional_TransELS.add_argument('--multi_exon_region_weight',type=str, default='regular',help="The weight in matrix A for multi_exon_region[default:regular][regular,minus_inner_region]")
-    optional_TransELS.add_argument('--output_matrix_info',type=str, default='False',help="Whether output matrix info [default:False] [True,False]")
-    optional_TransELS.add_argument('--normalize_sr_A',type=str, default='True',help="Whether normalize sr A [default:False] [True,False]")
-    optional_TransELS.add_argument('--sr_region_selection',type=str, default='real_data',help="SR region selection methods [default:real_data][read_length,num_exons,real_data]")
-    optional_TransELS.add_argument('--keep_sr_exon_region',type=str, default='True',help="Keep exon region for SR if using real data to filter region [default:True][True,False]")
+    # optional_TransELS.add_argument('-ref_genome','--reference_genome', type=str, help="The path of reference genome file",default=None)
+    # optional_TransELS.add_argument('--SR_quantification_option', type=str, help="SR quantification option[Options: Mili, Kallisto,Salmon, RSEM] [default:Kallisto]",default='Kallisto')
+    # # optional_TransELS.add_argument('--kallisto_index', type=str, help="Kallisto index",default='/fs/project/PCON0009/Yunhao/Project/Mili/Annotation/KallistoIndex/gencode.v39.transcripts.clean.dedup.m')
+    # optional_TransELS.add_argument('--alpha',type=str,default='adaptive', help="Alpha[default:adaptive]: SR and LR balance parameter")
+    # optional_TransELS.add_argument('--beta',type=str, default='1e-6',help="Beta[default:1e-6]: L2 regularization parameter")
+    # optional_TransELS.add_argument('--filtering',type=str,default='False', help="Whether the very short long reads will be filtered[default:False][True,False]")
+    # optional_TransELS.add_argument('--multi_mapping_filtering',type=str,default='best', help="How to filter multi-mapping reads[default:best][unique_only,best]")
+    # optional_TransELS.add_argument('--training',type=str,default='False', help="Generate training dict")
+    # optional_TransELS.add_argument('--DL_model',type=str,default=None,help='DL model to use')
+    # optional_TransELS.add_argument('--assign_unique_mapping_option',type=str,default='linear_model',help='How to assign unique mapping reads [Options:linear_model,manual_assign] [default:linear_model]')
+    # optional_TransELS.add_argument('-t','--threads',type=int, default=1,help="Number of threads")
+    # optional_TransELS.add_argument('--READ_JUNC_MIN_MAP_LEN',type=int, default=1,help="minimum mapped read length to consider a junction")
+    # optional_TransELS.add_argument('--use_weight_matrix',type=str, default='True',help="Whether use weight matrix[default:True][True,False]")
+    # optional_TransELS.add_argument('--normalize_lr_A',type=str, default='True',help="Whether normalize lr A [default:True] [True,False]")
+    # optional_TransELS.add_argument('--same_struc_isoform_handling',type=str, default='merge',help="How to handle isoforms with same structures within a gene[default:merge][merge,keep]")
+    # optional_TransELS.add_argument('--add_full_length_region',type=str, default='all',help="Whether add full length region[default:all] [all,nonfullrank,none]")
+    # optional_TransELS.add_argument('--multi_exon_region_weight',type=str, default='regular',help="The weight in matrix A for multi_exon_region[default:regular][regular,minus_inner_region]")
+    # optional_TransELS.add_argument('--output_matrix_info',type=str, default='False',help="Whether output matrix info [default:False] [True,False]")
+    # optional_TransELS.add_argument('--normalize_sr_A',type=str, default='True',help="Whether normalize sr A [default:False] [True,False]")
+    # optional_TransELS.add_argument('--sr_region_selection',type=str, default='real_data',help="SR region selection methods [default:real_data][read_length,num_exons,real_data]")
+    # optional_TransELS.add_argument('--keep_sr_exon_region',type=str, default='True',help="Keep exon region for SR if using real data to filter region [default:True][True,False]")
     # optional_TransELS.add_argument('--region_weight_path',type=str, default=weight_path,help="Mili LR region weight path")
    
     parser_EM = subparsers.add_parser('EM',help='Isoform quantification by EM algorith ')
@@ -111,7 +111,7 @@ def parse_arguments():
     optional_EM.add_argument('--iter_theta',type=str, default='False',help="Whether use updated theta to re-calculate conditional prob [True,False]")
     optional_EM.add_argument('--kde_path',type=str, default='/fs/project/PCON0009/Au-scratch2/haoran/_projects/long_reads_rna_seq_simulator/models/kde_H1-hESC_dRNA',help="KDE model path")
     optional_EM.add_argument('--eff_len_option',type=str, default='Kallisto',help="Calculation of effective length option [Kallisto,RSEM]")
-    optional_EM.add_argument('--EM_SR_num_iters',type=int, default=1000,help="Number of EM SR iterations")
+    optional_EM.add_argument('--EM_SR_num_iters',type=int, default=200,help="Number of EM SR iterations")
     optional_EM.add_argument('--alpha_df_path',type=str, default=None,help="Alpha df path")
     optional_EM.add_argument('--inital_theta',type=str, default='LR',help="inital_theta [LR,SR]")
 
@@ -151,59 +151,59 @@ def parse_arguments():
     if args.subparser_name in ['cal_K_value','TrEESR']:
         print('Calculate K values')
         TrEESR(args.gtf_annotation_path,args.output_path,args.short_read_sam_path,args.long_read_sam_path,args.sr_region_selection,args.filtering,args.threads,READ_JUNC_MIN_MAP_LEN=args.READ_JUNC_MIN_MAP_LEN)
-    elif args.subparser_name in ['quantify','TransELS']:
-        if args.training == 'True':
-            args.training = True
-        else:
-            args.training = False
-        print('Isoform quantification',flush=True)
-        if (args.short_read_sam_path is None) or (args.alpha == 1.0):
-            args.alpha = 1.0
-            args.SR_quantification_option = 'Mili'
-        if args.alpha != 1.0:
-            if args.short_read_sam_path is None:
-                raise Exception('You need to provide a short read alignemnt file if the alpha is not 1!')
-            if args.SR_quantification_option != 'Mili':
-                if (args.short_read_fastq is None) and (args.short_read_mate1_fastq is None or args.short_read_mate2_fastq is None):
-                    raise Exception('You need to provide the single end or paried end SR fastq if using other short read quantification options!')
-                if (args.reference_genome is None):
-                    raise Exception('You need to provide the reference genome if using other short read quantification options!')
-        if (args.alpha == 'adaptive'):
-            alpha = 'adaptive'
-        else:
-            try:
-                alpha = float(args.alpha)
-            except:
-                raise Exception('Alpha given is not numeric')
-        if (args.beta == 'adaptive'):
-            beta = 'adaptive'
-        else:
-            try:
-                beta = float(args.beta)
-            except:
-                raise Exception('Beta given is not numeric')
-        if args.SR_quantification_option not in ['Mili','Kallisto','Salmon','RSEM']:
-            raise Exception('SR_quantification_option is not valid.Options: [Mili, Kallisto,Salmon, RSEM]')
-        if (args.multi_mapping_filtering is None) or (not args.multi_mapping_filtering in ['unique_only','best']):
-            args.multi_mapping_filtering = 'no_filtering'
-        SR_fastq_list = []
-        if args.short_read_fastq is not None:
-            SR_fastq_list = [args.short_read_fastq]
-        elif args.short_read_mate1_fastq is not None:
-            SR_fastq_list = [args.short_read_mate1_fastq,args.short_read_mate2_fastq]
-        if args.DL_model is None:
-            args.DL_model = args.SR_quantification_option + '.pt'
-        TransELS(args.gtf_annotation_path,args.short_read_sam_path,args.long_read_sam_path,args.output_path,alpha,beta,1e-6,args.filtering,args.multi_mapping_filtering,args.SR_quantification_option,SR_fastq_list,args.reference_genome,args.training,args.DL_model,args.assign_unique_mapping_option,args.threads,READ_JUNC_MIN_MAP_LEN=args.READ_JUNC_MIN_MAP_LEN)
-    elif args.subparser_name in ['EM']:
+    # elif args.subparser_name in ['quantify','TransELS']:
+    #     if args.training == 'True':
+    #         args.training = True
+    #     else:
+    #         args.training = False
+    #     print('Isoform quantification',flush=True)
+    #     if (args.short_read_sam_path is None) or (args.alpha == 1.0):
+    #         args.alpha = 1.0
+    #         args.SR_quantification_option = 'Mili'
+    #     if args.alpha != 1.0:
+    #         if args.short_read_sam_path is None:
+    #             raise Exception('You need to provide a short read alignemnt file if the alpha is not 1!')
+    #         if args.SR_quantification_option != 'Mili':
+    #             if (args.short_read_fastq is None) and (args.short_read_mate1_fastq is None or args.short_read_mate2_fastq is None):
+    #                 raise Exception('You need to provide the single end or paried end SR fastq if using other short read quantification options!')
+    #             if (args.reference_genome is None):
+    #                 raise Exception('You need to provide the reference genome if using other short read quantification options!')
+    #     if (args.alpha == 'adaptive'):
+    #         alpha = 'adaptive'
+    #     else:
+    #         try:
+    #             alpha = float(args.alpha)
+    #         except:
+    #             raise Exception('Alpha given is not numeric')
+    #     if (args.beta == 'adaptive'):
+    #         beta = 'adaptive'
+    #     else:
+    #         try:
+    #             beta = float(args.beta)
+    #         except:
+    #             raise Exception('Beta given is not numeric')
+    #     if args.SR_quantification_option not in ['Mili','Kallisto','Salmon','RSEM']:
+    #         raise Exception('SR_quantification_option is not valid.Options: [Mili, Kallisto,Salmon, RSEM]')
+    #     if (args.multi_mapping_filtering is None) or (not args.multi_mapping_filtering in ['unique_only','best']):
+    #         args.multi_mapping_filtering = 'no_filtering'
+    #     SR_fastq_list = []
+    #     if args.short_read_fastq is not None:
+    #         SR_fastq_list = [args.short_read_fastq]
+    #     elif args.short_read_mate1_fastq is not None:
+    #         SR_fastq_list = [args.short_read_mate1_fastq,args.short_read_mate2_fastq]
+    #     if args.DL_model is None:
+    #         args.DL_model = args.SR_quantification_option + '.pt'
+    #     TransELS(args.gtf_annotation_path,args.short_read_sam_path,args.long_read_sam_path,args.output_path,alpha,beta,1e-6,args.filtering,args.multi_mapping_filtering,args.SR_quantification_option,SR_fastq_list,args.reference_genome,args.training,args.DL_model,args.assign_unique_mapping_option,args.threads,READ_JUNC_MIN_MAP_LEN=args.READ_JUNC_MIN_MAP_LEN)
+    elif args.subparser_name in ['quantify']:
         config.kde_path = args.kde_path
         if args.training == 'True':
             args.training = True
         else:
             args.training = False
-        print('Isoform quantification by EM algorithm',flush=True)
+        print('Isoform quantification by Mini',flush=True)
         if (args.short_read_sam_path is None) or (args.alpha == 1.0):
             args.alpha = 1.0
-            args.SR_quantification_option = 'Mili'
+            args.SR_quantification_option = 'Mini'
         # if args.alpha != 1.0:
         #     if args.short_read_sam_path is None:
         #         raise Exception('You need to provide a short read alignemnt file if the alpha is not 1!')
